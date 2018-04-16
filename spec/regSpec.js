29d8b29a9b41cb3de6ed12ba5a310dac3fb6f93b
Mila <liudmila03@gmail.com>
2018-04-16 14:30:49 +0100

    add updated files

--- spec/regSpec.js
+++ spec/regSpec.js
@@ -1,47 +1,10 @@
-describe("Registration", function() {
-    describe('Sussecfull Registration', function() {
-        it('should fail', function() {
-            expect(submitForm(1,1,1,1)).toBe(4);
+describe("validateForm", function() {
+    describe('Registration Notice', function() {
+        it("should have called the alert function if either field is undefined", function() {
+            spyOn(window, "alert");
+            validateForm("");
+            expect(window.alert).toHaveBeenCalledWith("Please fill in all information");
         });
-        it("Failed Registration", function() {
-            expect(submitForm(0,1,1,1)).toBe(3);
-        });
-        it("Failed Registration", function() {
-            expect(submitForm(0,0,1,1)).toBe(2);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(0,0,0,1)).toBe(1);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(0,0,0,0)).toBe(0);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,0,0,0)).toBe(1);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,1,0,0)).toBe(2);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,1,1,0)).toBe(3);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,0,1,0)).toBe(2);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,0,1,1)).toBe(3);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,1,0,1)).toBe(3);
-        });
-         it("Failed Registration", function() {
-            expect(submitForm(1,0,0,1)).toBe(2);
-        });
-          it("Failed Registration", function() {
-            expect(submitForm(0,1,0,0)).toBe(1);
-        });
-           it("Failed Registration", function() {
-            expect(submitForm(0,0,1,0)).toBe(1);
-        });
-
     });
 });
+       
