55a2f2a8d69cefcdde2a36826dcad6b7653a6d89
Mila <liudmila03@gmail.com>
2018-04-19 17:11:01 +0100

    links update

--- static/js/reg.js
+++ static/js/reg.js
@@ -1,7 +1,24 @@
-function validateForm() {
-    var field = document.forms["registrationForm"]["name","duration","mobile","email","date"].value;
-    if (field == "") {
-        alert("Please fill in all information");
-        return false;
-    };
-};
+function givePrice(){
+	var field = document.forms["registrationForm"]["duration"].value;
+	if (field >= 1 & field <= 4){
+        window.alert("Estimated Price for event 1000 Euros");
+    }
+    else if (field >= 5 & field <= 8){
+        window.alert("Estimated Price for event 2000 Euros");
+    }
+    else {
+        window.alert("Estimated Price for event 3000 Euros");
+    }
+}
+
+function checkDate(){
+	var d = document.forms["registrationForm"]["date"].value;
+	v = Date.parse(v);
+	var n = d.getDay();
+	if (n >= 1 & n <= 3){
+		window.alert("Sorry we do not work Monday - Wednesday");
+	}
+	else{
+		window.alert("Great! Looks like we are available!");
+	}
+}
--- tests/spec/regSpec.js
+++ tests/spec/regSpec.js
@@ -1,9 +1,19 @@
-describe("validateForm", function() {
-    describe('Registration Notice', function() {
-        it("should have called the alert function if either field is undefined", function() {
+describe("givePrice", function() {
+    describe('Price Estimation', function() {
+        it("should have called the alert function if field duratoin is provided", function() {
             spyOn(window, "alert");
-            validateForm("");
-            expect(window.alert).toHaveBeenCalledWith("Please fill in all information");
+            givePrice("3");
+            expect(window.alert).toHaveBeenCalledWith("Estimated Price is 1000 Euros");
+        });
+    });
+});
+
+describe("checkDate", function() {
+    describe('Free Date', function() {
+        it("should have called the alert function if date is provided", function() {
+            spyOn(window, "alert");
+            checkDate(0);
+            expect(window.alert).toHaveBeenCalledWith("Sorry we do not work Monday - Wednesday");
         });
     });
 });
