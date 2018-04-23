fa9bcff7a99b0886013ba293081e87e4259d78e9
Mila <liudmila03@gmail.com>
2018-04-23 09:54:20 +0100

    final commit

--- static/js/reg.js
+++ static/js/reg.js
@@ -1,24 +1,36 @@
-function givePrice(){
-	var field = document.forms["registrationForm"]["duration"].value;
+function givePrice(field){
 	if (field >= 1 & field <= 4){
-        window.alert("Estimated Price for event 1000 Euros");
+        return("Estimated Price for event 1000 Euros");
     }
     else if (field >= 5 & field <= 8){
-        window.alert("Estimated Price for event 2000 Euros");
+        return("Estimated Price for event 2000 Euros");
     }
     else {
-        window.alert("Estimated Price for event 3000 Euros");
+        return("Estimated Price for event 3000 Euros");
     }
 }
 
-function checkDate(){
-	var d = document.forms["registrationForm"]["date"].value;
-	d = Date.parse(d);
-	var n = d.getDay();
-	if (n >= 1 & n <= 3){
-		window.alert("Sorry we do not work Monday - Wednesday");
+function checkDate(d){
+	d = new Date(d);
+	d = d.getDay();
+	if (d >= 0 & d <= 3){
+		return("Sorry we do not work Monday - Wednesday");
 	}
 	else{
-		window.alert("Great! Looks like we are available!");
+		return("Great! Looks like we are available!");
 	}
+}
+
+function checkForm(field, d){
+	window.alert('Thank you for your booking!' + ' '+ givePrice(field) + ' ' + checkDate(d));
+}
+
+
+function myTopnav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
--- tests/spec/regSpec.js
+++ tests/spec/regSpec.js
@@ -1,19 +1,29 @@
-describe("givePrice", function() {
+/*describe("givePrice", function() {
     describe('Price Estimation', function() {
-        it("should have called the alert function if field duratoin is provided", function() {
-            spyOn(window, "alert");
+        it("should have called return function if field duratoin is provided", function() {
+            spyOn()
             givePrice("3");
-            expect(window.alert).toHaveBeenCalledWith("Estimated Price is 1000 Euros");
+            expect(return).toHaveBeenCalledWith("Estimated Price for event 1000 Euros");
         });
     });
-});
+});*/
 
-describe("checkDate", function() {
-    describe('Free Date', function() {
-        it("should have called the alert function if date is provided", function() {
-            spyOn(window, "alert");
-            checkDate(0);
-            expect(window.alert).toHaveBeenCalledWith("Sorry we do not work Monday - Wednesday");
+/*    describe('Free Date', function() {
+        it("should have called return function if date is provided", function() {
+            spyOn(console,'return');
+            checkDate(1);
+            expect(return).toHaveBeenCalledWith("Sorry we do not work Monday - Wednesday");
         });
     });
-});
+});*/
+
+describe("checkForm", function() {
+    describe ('Check Form', function(){
+        it("should have called alert function if duration and date are provided", function(){
+            spyOn(window, "alert");
+            checkForm("3",1);
+            expect(window.alert).toHaveBeenCalledWith('Thank you for your booking!' + ' '+ 'Estimated Price for event 1000 Euros' + ' ' + 'Sorry we do not work Monday - Wednesday')
+        })
+
+    })
+})
